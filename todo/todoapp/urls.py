ef6ed8bc3d052bd612afee6ccb973cc393b58b51
shameelvk <shameel3311@gmail.com>
2023-12-07 20:52:50 +0530

    comlpleted todo project

--- todo/todoapp/urls.py
+++ todo/todoapp/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.todo),
-    
+    path('', views.todo,name="main"),
+    path("todo-delete<int:item_id>",views.todo_delete,name="tododelete"),
+    path('clear-items/', views.clear_items, name='clear_items'),
+    path('todo-edit/<int:item_id>', views.edit_todo, name='edit_todo'),
+    path('todo-complete/<int:item_id>', views.todo_complete, name='complete_todo'),
 ]
--- todo/todoapp/views.py
+++ todo/todoapp/views.py
@@ -1,5 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from todoapp.models import TodoItem
 
 # Create your views here.
 def todo(request):
-    return render(request,'index.html')
+    if request.POST:
+       value=request.POST["item"]
+       new_todo=TodoItem(title=value)
+       new_todo.save()
+    todo_all=TodoItem.objects.all()
+    return render(request,'index.html',{'data':todo_all})
+def todo_delete(request,item_id):
+    ob=TodoItem.objects.get(id=item_id)
+    ob.delete()
+    return redirect("main")
+
+
+def clear_items(request):
+    TodoItem.objects.all().delete()
+    return redirect('main')
+
+def edit_todo(request,item_id):
+    if request.POST:
+        value=request.POST['edit']
+        obj=TodoItem.objects.get(id=item_id)
+        obj.title=value
+        obj.completed=False
+        obj.save()
+        return redirect("main")
+    ob=TodoItem.objects.get(id=item_id)
+    return render(request,'edit.html',{"data":ob})
+
+def todo_complete(request,item_id):
+    obj=TodoItem.objects.get(id=item_id)
+    obj.completed=True
+    obj.save()
+    return redirect('main')
